fad71ad7fcc90b44afc5eadb07cec5b0a54e63fb
giancarlo.bolzan <giancarlo.bolzan@MAQUINA-04>
2022-11-03 11:20:32 -0300

    Início de JavaScript

---
+++ JavaScript/scripts.js
@@ -0,0 +1,28 @@
+var atividade1 = document.getElementById("Atividade1");
+var atividade2 = document.getElementById("Atividade2");
+
+for (let i = 1; i < 10; i++)
+    if (i % 2 !== 0) {
+        var resposta1 = document.createElement("p");
+        resposta1.innerText = i;
+        atividade1.appendChild(resposta1);
+    }
+
+var soma = 0;
+
+for (let i = 1; i <= 100; i++)
+    soma += i
+
+var respostaSoma = document.createElement("p");
+respostaSoma.innerText = soma;
+atividade2.appendChild(respostaSoma);
+
+var contagemAlertas = 0;
+
+function cliqueBotao5Alertas() {
+    contagemAlertas++
+    alert(contagemAlertas)
+
+    if (contagemAlertas >= 5)
+        document.getElementById("botao5Alertas").disabled = true
+}
