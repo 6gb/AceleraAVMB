86c63ddce19599e851b26b4a53fe77627dde266b
6gb <63697672+6gb@users.noreply.github.com>
2022-11-10 22:02:41 -0300

    Modificações

--- HTMLCSSJS/js/scripts.js
+++ HTMLCSSJS/js/scripts.js
@@ -1,16 +1,3 @@
-/*
-fetch('section.html').then((resp) => resp.text()).then(function(data) {
-        document.getElementsByTagName("section").innerHTML = data;
-    });
-*/
-
-/*
-var xhr= new XMLHttpRequest();
-xhr.open('GET', 'section.html', true);
-xhr.onreadystatechange = function() {
-    if (this.readyState!==4) return;
-    if (this.status!==200) return; // or whatever error handling you want
-    document.getElementsByTagName('section')[0].innerHTML= this.responseText;
-};
-xhr.send();
-*/
+fetch('section.html')
+    .then((resposta) => resposta.text())
+    .then((data) => document.querySelector("section").innerHTML = data)
