a3c00be6a8d79be0d2cb3697552ec8f2de464c5d
giancarlo.bolzan <giancarlo.bolzan@MAQUINA-04>
2022-11-14 10:02:22 -0300

    Imagem interessante

--- HTMLCSSJS/js/scripts.js
+++ HTMLCSSJS/js/scripts.js
@@ -1,3 +1,11 @@
-fetch('section.html')
+fetch('index-section.html')
     .then((resposta) => resposta.text())
     .then((data) => document.querySelector("section").innerHTML = data)
+
+fetch('index-img.html')
+    .then((resposta) => resposta.text())
+    .then((data) => {
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = data;
+        document.querySelector("header").after(newDiv);
+    })
